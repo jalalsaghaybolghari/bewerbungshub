775ab4729e4edbeb16e1009ead88757990a0f8ec
Jalal saghaybolghari <jalalsaghaybolghari@gmail.com>
2026-08-27 17:00:07 +0200

    Wire up routing and providers

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+import './i18n'
+import { queryClient } from './lib/query-client'
+import { AuthProvider } from './auth/AuthContext'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
